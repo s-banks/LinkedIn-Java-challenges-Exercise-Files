714a1827be7452bbe4fd88081a860befd2dea752
Shane Banks <shane.banks02@gmail.com>
2022-09-14 17:57:57 -0500

    #3, updated

--- 01_03/begin/DoubleOrNothing/src/main/java/DoubleOrNothing.java
+++ 01_03/begin/DoubleOrNothing/src/main/java/DoubleOrNothing.java
@@ -9,15 +9,16 @@ public class DoubleOrNothing {
     public void playGame() {
         Scanner scanner = new Scanner(System.in);
         long points = 10;
-        System.out.println("Welcome to Double or Nothing. You begin with 10 points and\n have a chance to double it or loose it all each time you play.\n If you loos it all. the game is over");
+        System.out.println("Welcome to Double or Nothing. You begin with 10 points and\n have a chance to double it or loose it all each time you play.\n If you loose it all. the game is over");
         System.out.println("Ok, press \"ENTER\" to begin!");
         scanner.nextLine();
         while (points >= 0) {
             int random = randomNum();
             if (random > 0){
                 points = points * 2;
-                System.out.println("Lucky!\n");
+                System.out.println("Lucky!");
                 System.out.printf("You now have %d points\n", points);
+                System.out.println("Press \"ENTER\" to try again! ");
                 scanner.nextLine();
             } else {
                 points = 0;
