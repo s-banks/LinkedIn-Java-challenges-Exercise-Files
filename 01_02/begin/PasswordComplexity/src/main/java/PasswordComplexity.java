cecface916109b0e9117188f66ff7ee8c2fd25a9
Shane Banks <shane.banks02@gmail.com>
2022-09-14 16:32:15 -0500

    #2 done

--- 01_02/begin/PasswordComplexity/src/main/java/PasswordComplexity.java
+++ 01_02/begin/PasswordComplexity/src/main/java/PasswordComplexity.java
@@ -1,10 +1,18 @@
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class PasswordComplexity {
 
     public static boolean isPasswordComplex(String password) {
-        return true;
+        String regex = "^(?=.*[0-9])"
+                + "(?=.*[a-z])(?=.*[A-Z]).{6,200}$";
+        Pattern p = Pattern.compile(regex);
+        Matcher m = p.matcher(password);
+        return m.matches();
     }
+
+
 
     public static void main(String[] args) {
 
